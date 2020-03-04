0a9b24783aec8156a2f45781e0d6254fab406695
Carlos-Reiss <carlos_reis10.06@hotmail.com>
2020-03-04 11:15:37 -0300

    estilização global e Pages

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,56 @@
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Container = styled.h1`
+  max-width: 800px;
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  margin: 80px auto;
+  h1 {
+    font-size: 40px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 8px;
+    font-size: 16px;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  background: #7159c1;
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 5px;
+
+  #icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    animation: ${rotate} 3s backwards infinite;
+  }
+`;
